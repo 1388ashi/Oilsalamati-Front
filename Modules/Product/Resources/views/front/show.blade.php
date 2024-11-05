db77f325d4ef2d5d05255141fb49287002d6cc22
1388ashi <arshiabat5@gmail.com>
2024-11-05 15:05:08 +0330

    .

--- Modules/Product/Resources/views/front/show.blade.php
+++ Modules/Product/Resources/views/front/show.blade.php
@@ -50,8 +50,8 @@
                         <img
                             id="zoompro"
                             class="zoompro"
-                            src="{{$product->varieties[0]->images_showcase[0]->url}}"
-                            data-zoom-image="{{$product->varieties[0]->images_showcase[0]->url}}"
+                            {{-- src="{{$product->varieties[0]->images_showcase[0]->url}}"
+                            data-zoom-image="{{$product->varieties[0]->images_showcase[0]->url}}" --}}
                             alt="محصول"
                             width="625"
                             height="808"
