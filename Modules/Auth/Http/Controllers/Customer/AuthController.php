f5fb8ea90f3c6c7ecb2b9e803b503fa1840f1e4c
1388ashi <arshiabat5@gmail.com>
2024-11-05 16:30:06 +0330

    .

--- Modules/Auth/Http/Controllers/Customer/AuthController.php
+++ Modules/Auth/Http/Controllers/Customer/AuthController.php
@@ -269,7 +269,7 @@ class AuthController extends Controller
     }
     public function webLogin(CustomerLoginVerifyRequest $request)  
     {  
-        if ($request->password) {  
+        if ($request->smsToken) {  
             $request->smsToken->verified_at = now();  
             $request->smsToken->save();  
         }  
